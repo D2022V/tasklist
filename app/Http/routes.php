cd670cab072581243254770869df6c93479d7f74
D2022V <p963215875@protonmail.com>
2022-06-23 21:35:48 +0300

    auth and task relation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
+
+Route::get('/task', 'TaskController@index');
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
