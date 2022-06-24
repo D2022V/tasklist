b3934003e3da42653f202bb8fe5effa42decd5dd
D2022V <p963215875@protonmail.com>
2022-06-24 20:43:27 +0300

    controller TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Task;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $tasks = Auth::user()->tasks;
+        return view('tasks.index', ['tasks'=>$tasks]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|max:255'
+        ]);
+//        $user = $request->user();//first method
+//        $user = Auth::user();// second method
+        $request->user()->task()->create([
+            'name'=>$request->name,
+        ]);
+        return redirect(route(('task.index')));
+
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
